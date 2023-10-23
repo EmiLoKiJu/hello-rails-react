b8911faba4f1d6306250a70def713537b399c1e9
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-10-23 14:12:24 -0800

    Fix some linters

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,9 +3,9 @@ import '@hotwired/turbo-rails';
 import './controllers';
 import React from 'react';
 import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import store from './redux/store';
-import ReactDOM from 'react-dom/client';
 
 const root = document.getElementById('root');
 const rootContainer = ReactDOM.createRoot(root);
@@ -13,5 +13,5 @@ const rootContainer = ReactDOM.createRoot(root);
 rootContainer.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
